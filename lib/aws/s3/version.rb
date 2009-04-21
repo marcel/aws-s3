46273ad1d80606a45023548b208c1bb08eb50c24
Marcel Molina <marcel@marcelmolina.com>
2009-04-20 18:36:42 -0700

    Prepare CHANGELOG and version string for release.

--- lib/aws/s3/version.rb
+++ lib/aws/s3/version.rb
@@ -3,8 +3,8 @@ module AWS
     module VERSION #:nodoc:
       MAJOR    = '0'
       MINOR    = '6'
-      TINY     = '0' 
-      BETA     = Time.now.to_i.to_s
+      TINY     = '1' 
+      BETA     = #Time.now.to_i.to_s
     end
     
     Version = [VERSION::MAJOR, VERSION::MINOR, VERSION::TINY, VERSION::BETA].compact * '.'
