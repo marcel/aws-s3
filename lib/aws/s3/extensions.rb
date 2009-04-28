d8997822cfd4de9fe628f7478dbc11da623df976
Harry Love <harry.love@publicearth.com>
2009-04-28 15:35:29 -0600

    Fixes instance of Ruby version = 1.8.7 for Kernel method (for real)

--- lib/aws/s3/extensions.rb
+++ lib/aws/s3/extensions.rb
@@ -127,14 +127,14 @@ end
 module Kernel
   def __method__(depth = 0)
     caller[depth][/`([^']+)'/, 1]
-  end if RUBY_VERSION < '1.8.7'
+  end if RUBY_VERSION <= '1.8.7'
   
   def __called_from__
     caller[1][/`([^']+)'/, 1]
-  end if RUBY_VERSION >= '1.8.7'
+  end if RUBY_VERSION > '1.8.7'
   
   def expirable_memoize(reload = false, storage = nil)
-    current_method = RUBY_VERSION >= '1.8.7' ? __called_from__ : __method__(1)
+    current_method = RUBY_VERSION > '1.8.7' ? __called_from__ : __method__(1)
     storage = "@#{storage || current_method}"
     if reload 
       instance_variable_set(storage, nil)
