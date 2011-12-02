159984e956067fcb7d3c4bd54ab2b8daf10a9c43
Scott Jacobsen <scott@stackpilot.com>
2011-12-02 11:49:56 -0700

    Fix typos

--- lib/aws/s3/service.rb
+++ lib/aws/s3/service.rb
@@ -28,7 +28,7 @@ module AWS
         memoized :buckets
         
         # Sometimes methods that make requests to the S3 servers return some object, like a Bucket or an S3Object. 
-        # Othertimes they return just <tt>true</tt>. Other times they raise an exception that you may want to rescue. Despite all these 
+        # Other times they return just <tt>true</tt>. Other times they raise an exception that you may want to rescue. Despite all these 
         # possible outcomes, every method that makes a request stores its response object for you in Service.response. You can always 
         # get to the last request's response via Service.response.
         # 
