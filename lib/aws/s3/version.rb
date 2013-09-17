0fff2fc4eba4ec4880ab41d1dbc39b9b4072078a
Jeremy Shute <j@altschool.com>
2013-09-17 17:26:14 +0000

    bump minor version

--- lib/aws/s3/version.rb
+++ lib/aws/s3/version.rb
@@ -3,10 +3,10 @@ module AWS
     module VERSION #:nodoc:
       MAJOR    = '0'
       MINOR    = '6'
-      TINY     = '3'
+      TINY     = '4'
       BETA     = nil # Time.now.to_i.to_s
     end
-    
+
     Version = [VERSION::MAJOR, VERSION::MINOR, VERSION::TINY, VERSION::BETA].compact * '.'
   end
 end
