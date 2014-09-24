a0068e17a9af0bae723b397ce5059ad3a42b24cc
Ian Duggan <ian@ianduggan.net>
2014-09-24 13:12:38 -0700

    Add EHOSTUNREACH to list of errors to retry on.

--- lib/aws/s3/connection.rb
+++ lib/aws/s3/connection.rb
@@ -51,7 +51,7 @@ module AWS
         else
           http.start(&requester)
         end
-      rescue Errno::EPIPE, Timeout::Error, Errno::EINVAL, EOFError, Errno::ECONNRESET
+      rescue Errno::EPIPE, Timeout::Error, Errno::EINVAL, EOFError, Errno::ECONNRESET, Errno::EHOSTUNREACH
         @http = create_connection
         attempts == 3 ? raise : (attempts += 1; retry)
       end
