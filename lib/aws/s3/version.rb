61cf13637db13275f32501bcb1329a3a59ce75be
Nathaniel Rowe <nrowe@primerevenue.com>
2017-04-12 09:58:22 -0400

    Version bump

--- lib/aws/s3/version.rb
+++ lib/aws/s3/version.rb
@@ -3,7 +3,7 @@ module AWS
     module VERSION #:nodoc:
       MAJOR    = '0'
       MINOR    = '6'
-      TINY     = '3'
+      TINY     = '4'
       BETA     = nil # Time.now.to_i.to_s
     end
     
